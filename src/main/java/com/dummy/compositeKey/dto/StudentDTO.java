1b964f241029ff98b60d4e032a793fd4853ad6c8
Milan Zaric <zaric.milan11@gmail.com>
2023-12-25 13:20:44 +0100

    Added dto

---
+++ src/main/java/com/dummy/compositeKey/dto/StudentDTO.java
@@ -0,0 +1,15 @@
+package com.dummy.compositeKey.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+
+    private String name;
+
+
+}
---
+++ src/main/java/com/dummy/compositeKey/service/StudentServiceImpl.java
@@ -0,0 +1,4 @@
+package com.dummy.compositeKey.service;
+
+public class StudentServiceImpl {
+}
